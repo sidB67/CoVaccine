e759f8463662527ec26ca9d32af27c8512ee8775
sidB67 <sidbahl67@gmail.com>
2022-02-02 13:43:11 +0530

    UI changes and safety

--- lib/UI/district.dart
+++ lib/UI/district.dart
@@ -1,6 +1,7 @@
 import 'package:covaccine/UI/sessionUI.dart';
 import 'package:covaccine/UI/size_config.dart';
 import 'package:covaccine/providers/sessionsData.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
@@ -19,20 +20,17 @@ class _DistrictSearchState extends State<DistrictSearch> {
   var formatter = DateFormat('dd-MM-yyyy');
   String date = '';
   int year = 2021;
-  bool isStatesLoading = false;
+
   @override
   void initState() {
-   
     super.initState();
     date = formatter.format(DateTime.now());
     year = int.parse(formatter.format(DateTime.now()).split('-').last);
-    
-    
+
     setState(() {});
   }
-@override
- 
 
+  @override
   void _pickDateDialog() {
     showDatePicker(
             context: context,
@@ -110,177 +108,187 @@ class _DistrictSearchState extends State<DistrictSearch> {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: isStatesLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(
-                    top: SizeConfig.safeBlockVertical * 20,
-                    left: SizeConfig.safeBlockHorizontal * 10,
-                    right: SizeConfig.safeBlockHorizontal * 10,
-                  ),
-                  color: Colors.grey[200],
-                  width: double.infinity,
-                  height: SizeConfig.safeBlockVertical * 220,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Flexible(
-                        child: Container(
-                            height: SizeConfig.safeBlockVertical * 80,
-                            padding: EdgeInsets.only(left: 12),
-                            margin: EdgeInsets.only(
-                                bottom: SizeConfig.safeBlockVertical * 10,
-                                left: SizeConfig.safeBlockHorizontal * 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: DropdownButton<int>(
-                              underline: null,
-                              isDense: true,
-                              menuMaxHeight: SizeConfig.safeBlockVertical * 350,
-                              hint: Text('Select the State'),
-                              value: selectedState,
-                              items: getMenuItems(states),
-                              onChanged: (value) async {
-                                setState(() {
-                                  selectedState = value;
-                                  selectedDistrict = null;
-                                });
-                                await Provider.of<SessionsData>(context,
-                                        listen: false)
-                                    .getDistrict(value!);
-                              },
-                            )),
-                      ),
-                      Flexible(
-                        child: Container(
-                            height: SizeConfig.safeBlockVertical * 80,
-                            padding: EdgeInsets.only(left: 12),
-                            margin: EdgeInsets.only(
-                                bottom: SizeConfig.safeBlockVertical * 10,
-                                left: SizeConfig.safeBlockHorizontal * 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: DropdownButton<int>(
-                              hint: Text('Select the District'),
-                              underline: null,
-                              isDense: true,
-                              menuMaxHeight: SizeConfig.safeBlockVertical * 350,
-                              value: selectedDistrict,
-                              items: getDistrictItems(districts),
-                              onChanged: (value) {
-                                setState(() {
-                                  selectedDistrict = value;
-                                });
-                              },
-                            )),
-                      ),
-                      Flexible(
-                        child: GestureDetector(
-                          onTap: _pickDateDialog,
-                          child: Container(
-                              margin: EdgeInsets.only(
-                                  left: SizeConfig.safeBlockHorizontal * 10),
-                              width: SizeConfig.safeBlockHorizontal * 200,
-                              height: 48,
-                              padding: EdgeInsets.only(left: 12),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                '$date',
-                                style: TextStyle(fontSize: 16),
-                              )),
-                        ),
-                      ),
-                      Center(
-                        child: Container(
-                          margin: EdgeInsets.only(
-                              top: SizeConfig.safeBlockVertical * 20,
-                              bottom: SizeConfig.safeBlockVertical * 8),
-                          child: MaterialButton(
-                            color: Colors.greenAccent,
-                            shape: CircleBorder(),
-                            onPressed: () async {
-                              setState(() {
-                                isLoading = true;
-                              });
-                              try {
-                                await Provider.of<SessionsData>(context,
-                                        listen: false)
-                                    .getDistrictSessions(
-                                        selectedDistrict, date);
-                              } catch (e) {
-                                showDialog(
-                                    context: context,
-                                    builder: (_) {
-                                      return AlertDialog(
-                                        title: Text('Error Occured'),
-                                        content: Text(e.toString()),
-                                        actions: [
-                                          TextButton(
-                                              onPressed: () {
-                                                Navigator.pop(context);
-                                              },
-                                              child: Text('OK'))
-                                        ],
-                                      );
-                                    });
-                              } finally {
-                                setState(() {
-                                  isLoading = false;
-                                });
-                              }
-                            },
-                            child: Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Icon(Icons.search)),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+      body: NestedScrollView(
+        headerSliverBuilder: (ctx, scroll) {
+          return [
+            SliverToBoxAdapter(
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: SizeConfig.safeBlockVertical * 20,
+                  left: SizeConfig.safeBlockHorizontal * 10,
+                  right: SizeConfig.safeBlockHorizontal * 10,
                 ),
-                isLoading
-                    ? Expanded(
-                        child: Center(
-                        child: CircularProgressIndicator(),
-                      ))
-                    : Expanded(
-                        child: districtSessions.length == 0
-                            ? Text('No Match Found')
-                            : RefreshIndicator(
-                                onRefresh: () async {
-                                  await Provider.of<SessionsData>(context,
-                                          listen: false)
-                                      .getSessions(pincode, date);
-                                },
-                                child: ListView.builder(
-                                  itemCount: districtSessions.length,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    List s = districtSessions;
-                                    return Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: SessionUi(
-                                          session: s[index],
-                                        ));
-                                  },
-                                ),
-                              ),
+                color: Colors.grey[200],
+                width: double.infinity,
+                height: SizeConfig.safeBlockVertical * 220,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Flexible(
+                      child: Container(
+                          height: SizeConfig.safeBlockVertical * 80,
+                          padding: EdgeInsets.only(left: 12),
+                          margin: EdgeInsets.only(
+                              bottom: SizeConfig.safeBlockVertical * 10,
+                              left: SizeConfig.safeBlockHorizontal * 10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: DropdownButton<int>(
+                            underline: null,
+                            isDense: true,
+                            menuMaxHeight: SizeConfig.safeBlockVertical * 350,
+                            hint: Text('Select the State'),
+                            value: selectedState,
+                            items: getMenuItems(states),
+                            onChanged: (value) async {
+                              setState(() {
+                                selectedState = value;
+                                selectedDistrict = null;
+                              });
+                              await Provider.of<SessionsData>(context,
+                                      listen: false)
+                                  .getDistrict(value!);
+                            },
+                          )),
+                    ),
+                    Flexible(
+                      child: Container(
+                          height: SizeConfig.safeBlockVertical * 80,
+                          padding: EdgeInsets.only(left: 12),
+                          margin: EdgeInsets.only(
+                              bottom: SizeConfig.safeBlockVertical * 10,
+                              left: SizeConfig.safeBlockHorizontal * 10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: DropdownButton<int>(
+                            hint: Text('Select the District'),
+                            underline: null,
+                            isDense: true,
+                            menuMaxHeight: SizeConfig.safeBlockVertical * 350,
+                            value: selectedDistrict,
+                            items: getDistrictItems(districts),
+                            onChanged: (value) {
+                              setState(() {
+                                selectedDistrict = value;
+                              });
+                            },
+                          )),
+                    ),
+                    Flexible(
+                      child: GestureDetector(
+                        onTap: _pickDateDialog,
+                        child: Container(
+                            margin: EdgeInsets.only(
+                                left: SizeConfig.safeBlockHorizontal * 10),
+                            width: SizeConfig.safeBlockHorizontal * 200,
+                            height: 48,
+                            padding: EdgeInsets.only(left: 12),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              '$date',
+                              style: TextStyle(fontSize: 16),
+                            )),
                       ),
-              ],
-            ),
+                    ),
+                    Center(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            top: SizeConfig.safeBlockVertical * 20,
+                            bottom: SizeConfig.safeBlockVertical * 8),
+                        child: MaterialButton(
+                          color: (selectedState !=null) && (selectedDistrict!=null) == false
+                              ? Colors.grey
+                              : Colors.greenAccent,
+                          shape: CircleBorder(),
+                          onPressed: (selectedState ==null) && (selectedDistrict==null) == false
+                              ? () {}
+                              : () async {
+                                  setState(() {
+                                    isLoading = true;
+                                  });
+                                  try {
+                                    await Provider.of<SessionsData>(context,
+                                            listen: false)
+                                        .getDistrictSessions(
+                                            selectedDistrict, date);
+                                  } catch (e) {
+                                    showDialog(
+                                        context: context,
+                                        builder: (_) {
+                                          return AlertDialog(
+                                            title: Text('Error Occured'),
+                                            content: Text(
+                                                'Please enter all the values'),
+                                            actions: [
+                                              TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: Text('OK'))
+                                            ],
+                                          );
+                                        });
+                                  } finally {
+                                    setState(() {
+                                      isLoading = false;
+                                    });
+                                  }
+                                },
+                          child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Icon(Icons.search)),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ];
+        },
+        body: isLoading
+            ? Container(
+                child: Center(
+                child: CircularProgressIndicator(),
+              ))
+            : Container(
+                alignment: Alignment.center,
+                child: districtSessions.length == 0
+                    ? Text(
+                        'No Match Found',
+                        style: TextStyle(
+                            fontSize: SizeConfig.safeBlockVertical * 20,
+                            fontWeight: FontWeight.bold),
+                      )
+                    : RefreshIndicator(
+                        onRefresh: () async {
+                          await Provider.of<SessionsData>(context,
+                                  listen: false)
+                              .getSessions(pincode, date);
+                        },
+                        child: ListView.builder(
+                          itemCount: districtSessions.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            List s = districtSessions;
+                            return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SessionUi(
+                                  session: s[index],
+                                ));
+                          },
+                        ),
+                      ),
+              ),
+      ),
     );
   }
 }
