176b07bd68edc525d6739c6d63690bdacdfbf3c4
sidB67 <sidbahl67@gmail.com>
2022-02-02 09:46:45 +0530

    added search by district

--- lib/UI/district.dart
+++ lib/UI/district.dart
@@ -71,13 +71,28 @@ class _DistrictSearchState extends State<DistrictSearch> {
     return items;
   }
 
+  List<DropdownMenuItem<int>> getDistrictItems(List<District> district) {
+    List<DropdownMenuItem<int>> items = [];
+    district.forEach((element) {
+      items.add(
+        DropdownMenuItem(
+          child: Text(element.districtName),
+          value: element.districtId,
+        ),
+      );
+    });
+
+    return items;
+  }
+
   var selectedState;
+  var selectedDistrict;
   bool isLoading = false;
   @override
   Widget build(BuildContext context) {
-    final sessions = Provider.of<SessionsData>(context).session;
+    final districtSessions = Provider.of<SessionsData>(context).districtSessions;
     final states = Provider.of<SessionsData>(context).states;
-
+    final districts = Provider.of<SessionsData>(context).districts;
     SizeConfig().init(context);
     print('build');
     return Scaffold(
@@ -106,111 +121,126 @@ class _DistrictSearchState extends State<DistrictSearch> {
             ),
             color: Colors.grey[200],
             width: double.infinity,
-            height: SizeConfig.safeBlockVertical * 180,
+            height: SizeConfig.safeBlockVertical * 220,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Row(
-                  children: [
-                    Flexible(
-                      child: Container(
-                          // height: 60,
-                          padding: EdgeInsets.only(left: 12),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: DropdownButton<int>(
-                            hint: Text('Select the State'),
-                            value: selectedState,
-                            items: getMenuItems(states),
-                            onChanged: (value){
-                              setState(() {
-                                selectedState = value;
-                              });
-                            },
-                          )),
-                    ),
-                    SizedBox(width: 10),
-                    // Expanded(
-                    //   child: Column(
-                    //     // mainAxisAlignment: MainAxisAlignment.start,
-                    //     crossAxisAlignment: CrossAxisAlignment.start,
-                    //     children: [
-                    //       Padding(
-                    //         padding: const EdgeInsets.only(bottom: 8.0),
-                    //         child: FittedBox(
-                    //           child: Text(
-                    //             "Enter Date in the given format",
-                    //             style: TextStyle(
-                    //                 color: Colors.black.withOpacity(.5),
-                    //                 fontSize: 14),
-                    //             textAlign: TextAlign.center,
-                    //           ),
-                    //         ),
-                    //       ),
-                    //       // SizedBox(height:10),
-                    //       GestureDetector(
-                    //         onTap: _pickDateDialog,
-                    //         child: Container(
-                    //           width: double.infinity,
-                    //           height: 48,
-                    //           padding: EdgeInsets.only(left: 12),
-                    //           decoration: BoxDecoration(
-                    //             color: Colors.white,
-                    //             borderRadius: BorderRadius.circular(20),
-                    //           ),
-                    //           alignment: Alignment.centerLeft,
-                    //           child: Text(
-                    //             '$date',
-                    //             style: TextStyle(
-                    //               fontSize: 16
-                    //             ),
-                    //           )
-                    //         ),
-                    //       ),
-                    //     ],
-                    //   ),
-                    // ),
-                  ],
+                Flexible(
+                  child: Container(
+                      height: SizeConfig.safeBlockVertical * 80,
+                      padding: EdgeInsets.only(left: 12),
+                      margin: EdgeInsets.only(
+                          bottom: SizeConfig.safeBlockVertical * 10,
+                          left: SizeConfig.safeBlockHorizontal * 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: DropdownButton<int>(
+                        underline: null,
+                        isDense: true,
+                        menuMaxHeight: SizeConfig.safeBlockVertical * 350,
+                        hint: Text('Select the State'),
+                        value: selectedState,
+                        items: getMenuItems(states),
+                        onChanged: (value) async {
+                          setState(() {
+                            selectedState = value;
+                            selectedDistrict = null;
+                          });
+                          await Provider.of<SessionsData>(context,
+                                  listen: false)
+                              .getDistrict(value!);
+                        },
+                      )),
                 ),
-                Container(
-                  margin:
-                      EdgeInsets.only(top: SizeConfig.safeBlockVertical * 20),
-                  child: MaterialButton(
-                    color: Colors.greenAccent,
-                    shape: CircleBorder(),
-                    onPressed: () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      try {
-                        await Provider.of<SessionsData>(context, listen: false)
-                            .getSessions(pincode, date);
-                      } catch (e) {
-                        showDialog(
-                            context: context,
-                            builder: (_) {
-                              return AlertDialog(
-                                title: Text('Error Occured'),
-                                content: Text(e.toString()),
-                                actions: [
-                                  TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text('OK'))
-                                ],
-                              );
-                            });
-                      } finally {
+                Flexible(
+                  child: Container(
+                      height: SizeConfig.safeBlockVertical * 80,
+                      padding: EdgeInsets.only(left: 12),
+                      margin: EdgeInsets.only(
+                          bottom: SizeConfig.safeBlockVertical * 10,
+                          left: SizeConfig.safeBlockHorizontal * 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: DropdownButton<int>(
+                        hint: Text('Select the District'),
+                        underline: null,
+                        isDense: true,
+                        menuMaxHeight: SizeConfig.safeBlockVertical * 350,
+                        value: selectedDistrict,
+                        items: getDistrictItems(districts),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedDistrict = value;
+                          });
+                        },
+                      )),
+                ),
+                Flexible(
+                  child: GestureDetector(
+                    onTap: _pickDateDialog,
+                    child: Container(
+                        margin: EdgeInsets.only(
+                            left: SizeConfig.safeBlockHorizontal * 10),
+                        width: SizeConfig.safeBlockHorizontal * 200,
+                        height: 48,
+                        padding: EdgeInsets.only(left: 12),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '$date',
+                          style: TextStyle(fontSize: 16),
+                        )),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    margin:
+                        EdgeInsets.only(top: SizeConfig.safeBlockVertical * 20,bottom: SizeConfig.safeBlockVertical * 8),
+                    child: MaterialButton(
+                      color: Colors.greenAccent,
+                      shape: CircleBorder(),
+                      onPressed: () async {
                         setState(() {
-                          isLoading = false;
+                          isLoading = true;
                         });
-                      }
-                    },
-                    child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Icon(Icons.search)),
+                        try {
+                          await Provider.of<SessionsData>(context,
+                                  listen: false)
+                              .getDistrictSessions(selectedDistrict, date);
+                        } catch (e) {
+                          showDialog(
+                              context: context,
+                              builder: (_) {
+                                return AlertDialog(
+                                  title: Text('Error Occured'),
+                                  content: Text(e.toString()),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text('OK'))
+                                  ],
+                                );
+                              });
+                        } finally {
+                          setState(() {
+                            isLoading = false;
+                          });
+                        }
+                      },
+                      child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Icon(Icons.search)),
+                    ),
                   ),
                 )
               ],
@@ -222,7 +252,7 @@ class _DistrictSearchState extends State<DistrictSearch> {
                   child: CircularProgressIndicator(),
                 ))
               : Expanded(
-                  child: sessions.length == 0
+                  child: districtSessions.length == 0
                       ? Text('No Match Found')
                       : RefreshIndicator(
                           onRefresh: () async {
@@ -231,9 +261,9 @@ class _DistrictSearchState extends State<DistrictSearch> {
                                 .getSessions(pincode, date);
                           },
                           child: ListView.builder(
-                            itemCount: sessions.length,
+                            itemCount: districtSessions.length,
                             itemBuilder: (BuildContext context, int index) {
-                              List s = sessions;
+                              List s = districtSessions;
                               return Padding(
                                   padding: const EdgeInsets.all(8.0),
                                   child: SessionUi(
--- lib/providers/sessionsData.dart
+++ lib/providers/sessionsData.dart
@@ -3,18 +3,30 @@ import 'dart:convert';
 import 'package:covaccine/models/sessions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-class States{
+
+class States {
   final int stateId;
   final stateName;
   States({
-     required this.stateId,
-     this.stateName,
+    required this.stateId,
+    this.stateName,
   });
-  }
+}
+
+class District {
+  final int districtId;
+  final districtName;
+  District({
+    required this.districtId,
+    this.districtName,
+  });
+}
 
 class SessionsData with ChangeNotifier {
   List _sessions = [];
   List _states = [];
+  List _districts = [];
+  List _districtSessions=[];
   Future<void> getSessions(String pincode, String date) async {
     print('called');
     final url = Uri.parse(
@@ -39,43 +51,105 @@ class SessionsData with ChangeNotifier {
     }
     notifyListeners();
   }
+
   Future<void> getStates() async {
     print('called');
-    final url = Uri.parse(
-        "https://cdn-api.co-vin.in/api/v2/admin/location/states");
+    final url =
+        Uri.parse("https://cdn-api.co-vin.in/api/v2/admin/location/states");
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
         final responseData = jsonDecode(response.body);
-        final extractedData = responseData["states"] ;
-        final loadedSessions = extractedData.forEach((element) { 
+        final extractedData = responseData["states"];
+        final loadedSessions = extractedData.forEach((element) {
           _states.add(States(
             stateId: element["state_id"],
             stateName: element["state_name"],
           ));
         });
-            
-        
       } else {
         final responseData = jsonDecode(response.body);
         var error = responseData["error"];
         throw error;
       }
 
-     _states.forEach((element) {
-       print(element.stateName);
-     });
+      _states.forEach((element) {
+        print(element.stateName);
+      });
     } catch (e) {
       throw e;
     }
     notifyListeners();
   }
 
+  Future<void> getDistrict(int state_id) async {
+    List loadedData = [];
+    print('called');
+    final url = Uri.parse(
+        "https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state_id}");
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        final extractedData = responseData["districts"];
+        _districts.clear();
+        final loadedSessions = extractedData.forEach((element) {
+          loadedData.add(District(
+            districtId: element["district_id"],
+            districtName: element["district_name"],
+          ));
+        });
+      } else {
+        final responseData = jsonDecode(response.body);
+        var error = responseData["error"];
+        throw error;
+      }
+      _districts = loadedData;
+      _districts.forEach((element) {
+        print(element.districtName);
+      });
+    } catch (e) {
+      throw e;
+    }
+    notifyListeners();
+  }
+  Future<void> getDistrictSessions(int district_id, String date) async {
+    print('called');
+    final url = Uri.parse(
+        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=$district_id&date=$date");
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        Iterable extractedData = responseData["sessions"];
+        List loadedSessions =
+            extractedData.map((json) => Session.fromJson(json)).toList();
+        _districtSessions = loadedSessions;
+      } else {
+        final responseData = jsonDecode(response.body);
+        var error = responseData["error"];
+        throw error;
+      }
+
+      print(_districtSessions.length);
+    } catch (e) {
+      print(e);
+      throw e;
+    }
+    notifyListeners();
+  }
+  
   List<Session> get session {
     return [..._sessions];
   }
+
   List<States> get states {
     return [..._states];
   }
+  List<District> get districts {
+    return [..._districts];
+  }
+  List<Session> get districtSessions {
+    return [..._districtSessions];
+  }
 }
-
